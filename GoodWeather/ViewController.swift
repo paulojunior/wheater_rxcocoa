d8fe0db5a30b2f0671092f469d78d356905088c9
Paulo Ferreira <paulo.ferreira@ditech.com.br>
2021-08-20 20:19:27 -0300

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Junior Ferreira on 20/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var tempetureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
