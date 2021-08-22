12852df14819eacd85ae8527e83f69d2ed651c26
Paulo Ferreira <paulo.ferreira@ditech.com.br>
2021-08-21 21:32:48 -0300

    Handle Errors with Catch

--- GoodWeather/ViewController.swift
+++ GoodWeather/ViewController.swift
@@ -55,10 +55,12 @@ class ViewController: UIViewController {
         }
         
         let resource = Resource<WeatherResult>(url: url)
-        
         let search = URLRequest.load(resource: resource)
             .observeOn(MainScheduler.instance)
-            .asDriver(onErrorJustReturn: WeatherResult.empty)
+            .catchError { error in
+                print(error.localizedDescription)
+                return Observable.just(WeatherResult.empty)
+            }.asDriver(onErrorJustReturn: WeatherResult.empty)
         
         search.map { "\($0.main.temp)" }
             .drive(self.tempetureLabel.rx.text)
